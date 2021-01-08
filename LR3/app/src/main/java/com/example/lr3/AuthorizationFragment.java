b50f7f7b4d77b1b0145c87934b7748751169318e
igor <igortiger777@mail.ru>
2021-01-09 00:48:02 +0300

    lr3

---
+++ LR3/app/src/main/java/com/example/lr3/AuthorizationFragment.java
@@ -0,0 +1,24 @@
+package com.example.lr3;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+
+public class AuthorizationFragment extends Fragment
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_authorization, container, false);
+    }
+}
